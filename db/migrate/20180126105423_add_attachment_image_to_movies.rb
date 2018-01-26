7ca83730997164eb6d96ab0d0fd54d3d0568aae6
Katherine Worley <kdesign216@yahoo.com>
2018-01-26 05:56:54 -0500

    added attachment image to movies

---
+++ db/migrate/20180126105423_add_attachment_image_to_movies.rb
@@ -0,0 +1,11 @@
+class AddAttachmentImageToMovies < ActiveRecord::Migration[5.1]
+  def self.up
+    change_table :movies do |t|
+      t.attachment :image
+    end
+  end
+
+  def self.down
+    remove_attachment :movies, :image
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180125153928) do
+ActiveRecord::Schema.define(version: 20180126105423) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 20180125153928) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
