1179356b2b1f103ebf07e4a17c4aaa7e99d0a178
Katherine Worley <kdesign216@yahoo.com>
2018-01-30 06:02:23 -0500

    added user id to reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180130104115) do
+ActiveRecord::Schema.define(version: 20180130110146) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20180130104115) do
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
